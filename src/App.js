f1312b22476bac3b4266f8feb117380bb4522526
konstantin b <bogucharov@gmail.com>
2019-09-17 00:28:27 +0400

    Removed old import style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Footer from 'components/layout/Footer';
-import './style.scss';
 
 const reactMessage = 'Hello React!';
 
